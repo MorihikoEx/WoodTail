77f8053dcfc47ebbe81b888c1109df67aa370e64
morihiko <morihiko0701@outlook.com>
2025-01-07 06:40:40 +0800

    all

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -6,11 +6,9 @@ defineProps<{h1: string ; h2: string }>()
   <h1>{{ h1 }}</h1>
   <h2>{{ h2 }}</h2>
   <p class="light">更简约，更温和，更容易</p>
-  <p class="light"><s>以及会让所有前端工程师都发出尖锐爆鸣的不规范无结构</s></p>
+  <p class="light">以及更绝美的精神状态</p>
 </template>
 
 <style scoped>
-h2{
-  margin-top: -1em;
-}
+
 </style>
